8d41e89a63da404f7073d390d9be501292ccf2d1
Daniel Grbac Bravo <daniel@Daniels-MacBook-Pro.local>
2023-01-30 13:53:26 +0300

    openai api addition and call

--- Transformer.py
+++ Transformer.py
@@ -3,6 +3,11 @@ from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTok
 import torch
 from PIL import Image
 
+import openai
+from os import getenv
+
+import apiconfigparser
+
 model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
@@ -33,4 +38,22 @@ def predict_step(image_paths):
   preds = [pred.strip() for pred in preds]
   return preds
 
-print(predict_step(['Unknown.jpeg']))
+imagedescriptor = predict_step(['1595526028946.jpeg'])[0]
+print(imagedescriptor)
+#function that uses the openai api to generate a poem using the imagedecriptor as the prompt
+def generate_poem(imagedescriptor):
+    openai.api_key = apiconfigparser.get_openai_api_key()
+    response = openai.Completion.create(
+      engine="davinci",
+      prompt= " image caption ["+ imagedescriptor + '] n/ write a more detailed description based on the caption: ',
+      max_tokens=100,
+      temperature=0.9,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0.6,
+      stop=["\n"]
+    )
+    return response.choices[0].text
+
+
+print(generate_poem(imagedescriptor))
